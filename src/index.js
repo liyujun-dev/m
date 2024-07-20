3113e64c2d2ebf44271607554f45e379bb6a6cb9
Yujun Li <liyujun@zohomail.com>
2024-07-21 00:04:55 +0800

    feat: Using nginx default page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import INDEX_HTML from './index.html';
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		return new Response(
+		  INDEX_HTML,
+		  {
+		    headers: {
+		      'Content-Type': 'text/html; charset=UTF-8'
+		    }
+		  }
+		);
 	},
 };
