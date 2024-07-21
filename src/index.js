3bb586ccb4c0b93c4316afde8751fe723a7fc979
Yujun Li <liyujun@zohomail.com>
2024-07-21 14:03:35 +0800

    chore: Editor settings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import INDEX_HTML from './index.html';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response(
-		  INDEX_HTML,
-		  {
-		    headers: {
-		      'Content-Type': 'text/html; charset=UTF-8'
-		    }
-		  }
-		);
-	},
+  async fetch(request, env, ctx) {
+    if (request.headers.get('Accept')?.includes('html')) {
+      return new Response(INDEX_HTML, {
+        headers: {
+          'Content-Type': 'text/html; charset=UTF-8',
+        },
+      });
+    }
+    console.log(request);
+    return Response.json({ message: 'ok' });
+  },
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,18 +3,36 @@ import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
 describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+  it('responds with index html! (unit style)', async () => {
+    const request = new Request('http://example.com', { headers: { Accept: 'text/html' } });
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    await waitOnExecutionContext(ctx);
+    expect(await response.text()).toMatchInlineSnapshot(`
+			"<!DOCTYPE html>
+			<html>
+			<head>
+			<title>Welcome to nginx!</title>
+			<style>
+			html { color-scheme: light dark; }
+			body { width: 35em; margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif; }
+			</style>
+			</head>
+			<body>
+			<h1>Welcome to nginx!</h1>
+			<p>If you see this page, the nginx web server is successfully installed and
+			working. Further configuration is required.</p>
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch(request, env, ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+			<p>For online documentation and support please refer to
+			<a href="http://nginx.org/">nginx.org</a>.<br/>
+			Commercial support is available at
+			<a href="http://nginx.com/">nginx.com</a>.</p>
+
+			<p><em>Thank you for using nginx.</em></p>
+			</body>
+			</html>
+			"
+		`);
+  });
 });
