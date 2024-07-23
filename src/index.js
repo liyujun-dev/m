c74ea145b62f5e3653c3bf8e507ce3d223719016
Yujun Li <liyujun@zohomail.com>
2024-07-23 17:57:09 +0800

    feat: Support pull image from docker hub

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import INDEX_HTML from './index.html';
 
+/** @type {string[]} */
+const registries = ['registry-1.docker.io'];
+
 export default {
+  /**
+   * Asynchronously fetches data based on the request and environment.
+   *
+   * @param {Request} request - The request object for fetching data.
+   * @param {Object} env - The environment settings.
+   * @param {Object} ctx - The context of the fetch operation.
+   * @return {Promise<Response>} The response object containing the fetched data.
+   */
   async fetch(request, env, ctx) {
-    if (request.headers.get('Accept')?.includes('html')) {
-      return new Response(INDEX_HTML, {
+    const url = new URL(request.url);
+    const workerUrl = `https://${url.hostname}/`;
+    const registry = registries[0];
+
+    if (url.pathname === '/') {
+      return new Response(INDEX_HTML, { headers: { 'Content-Type': 'text/html' } });
+    }
+    if (url.searchParams.has('scope') || url.searchParams.has('service')) {
+      const [_, authHostname, authPathname] = url.pathname.split('/');
+      url.hostname = authHostname;
+      url.pathname = authPathname;
+      return await fetch(new Request(url.href, request));
+    }
+
+    url.hostname = registry;
+    const res = await fetch(new Request(url, request));
+    if (res.headers.get('Www-Authenticate')) {
+      return new Response(res.body, {
+        status: res.status,
         headers: {
-          'Content-Type': 'text/html; charset=UTF-8',
+          ...res.headers,
+          'Www-Authenticate': res.headers.get('Www-Authenticate').replace(/https?:\/\//, workerUrl),
         },
       });
     }
-    console.log(request);
-    return Response.json({ message: 'ok' });
+    return res;
   },
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,38 +1,46 @@
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
-describe('Hello World worker', () => {
-  it('responds with index html! (unit style)', async () => {
-    const request = new Request('http://example.com', { headers: { Accept: 'text/html' } });
-    const ctx = createExecutionContext();
-    const response = await worker.fetch(request, env, ctx);
-    await waitOnExecutionContext(ctx);
-    expect(await response.text()).toMatchInlineSnapshot(`
-			"<!DOCTYPE html>
-			<html>
-			<head>
-			<title>Welcome to nginx!</title>
-			<style>
-			html { color-scheme: light dark; }
-			body { width: 35em; margin: 0 auto;
-			font-family: Tahoma, Verdana, Arial, sans-serif; }
-			</style>
-			</head>
-			<body>
-			<h1>Welcome to nginx!</h1>
-			<p>If you see this page, the nginx web server is successfully installed and
-			working. Further configuration is required.</p>
+it('hello', async () => {
+  const res = await fetch('https://example.com/');
+  expect(res.status).toEqual(200);
+  expect(await res.text()).toMatchSnapshot();
+});
 
-			<p>For online documentation and support please refer to
-			<a href="http://nginx.org/">nginx.org</a>.<br/>
-			Commercial support is available at
-			<a href="http://nginx.com/">nginx.com</a>.</p>
-
-			<p><em>Thank you for using nginx.</em></p>
-			</body>
-			</html>
-			"
-		`);
+it('Pull image from docker hub', async () => {
+  const headers = new Headers({
+    'accept-encoding': 'gzip, br',
+    'user-agent':
+      'docker/24.0.7 go/go1.20.10 git-commit/311b9ff kernel/6.6.9-200.fc39.x86_64 os/linux arch/amd64 UpstreamClient(Docker-Client/24.0.7 \\(linux\\))',
   });
+  const v2Req = new Request('https://example.com/v2/', { headers });
+  const v2Res = await worker.fetch(v2Req);
+  expect(v2Res.status).toEqual(401);
+  expect(v2Res.headers.get('Www-Authenticate')).toMatchSnapshot();
+
+  const tokenReq = new Request(
+    'https://example.com/auth.docker.io/token/?scope=repository%3Alibrary%2Falpine%3Apull&service=registry.docker.io',
+    { headers }
+  );
+  const tokenRes = await worker.fetch(tokenReq);
+  expect(tokenRes.status).toEqual(200);
+  const tokenResult = await tokenRes.json();
+  expect(tokenResult).toHaveProperty('access_token');
+  expect(tokenResult).toHaveProperty('expires_in');
+  expect(tokenResult).toHaveProperty('issued_at');
+  expect(tokenResult).toHaveProperty('token');
+
+  const manifestReq = new Request('https://example.com/v2/library/alpine/manifests/latest', {
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${tokenResult.access_token}`,
+    },
+  });
+  const manifestRes = await worker.fetch(manifestReq);
+  expect(manifestRes.status).toEqual(200);
+  const manifestResult = await manifestRes.json();
+  expect(manifestResult).toHaveProperty('name');
+  expect(manifestResult).toHaveProperty('fsLayers');
+  expect(manifestResult).toHaveProperty('history');
+  expect(manifestResult).toHaveProperty('tag');
 });
