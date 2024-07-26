b6b72c7c3828a5de6470d885c8b934337def0ab9
Yujun Li <liyujun@zohomail.com>
2024-07-26 17:11:42 +0800

    refactor: Support any registry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import INDEX_HTML from './index.html';
 
-/** @type {string[]} */
-const registries = ['registry-1.docker.io', 'quay.io', 'ghcr.io', 'gcr.io', 'registry.k8s.io'];
-
 export default {
   /**
    * Asynchronously fetches data based on the request and environment.
@@ -14,35 +11,39 @@ export default {
    */
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    console.log(url);
-    const workerUrl = `https://${url.hostname}/`;
     if (url.pathname === '/') {
       return new Response(INDEX_HTML, { headers: { 'Content-Type': 'text/html' } });
     }
-    if (url.searchParams.has('scope') || url.searchParams.has('service')) {
-      const [_, authHostname, ...authPathname] = url.pathname.split('/');
-      url.hostname = authHostname;
-      url.pathname = authPathname.join('/');
-      return await fetch(new Request(url.href, request));
+    if (url.pathname === '/v2/') {
+      return Response.json({});
     }
 
-    if (url.pathname.startsWith('/v2') && url.pathname.includes('.')) {
-      url.pathname = url.pathname.replace('/v2', '');
+    url.hostname = 'registry-1.docker.io';
+    if (!url.pathname.startsWith('/v2/library') && !url.pathname.startsWith('/v2/docker.io')) {
+      const registry = url.pathname.split('/')[2];
+      url.hostname = registry;
+      url.pathname = url.pathname.replace(`${registry}/`, '');
+    }
+    if (url.pathname.startsWith('/v2/docker.io')) {
+      url.pathname = url.pathname.replace('docker.io/', '');
     }
 
-    const registry = registries.find((r) => url.pathname.startsWith(`/${r}`)) ?? registries[0];
-    url.hostname = registry;
-    url.pathname = url.pathname.replace(`/${registry}`, '');
-    console.log(url);
-    const res = await fetch(new Request(url, request));
+    const req = new Request(url, request);
+    let res = await fetch(req);
     if (res.headers.get('Www-Authenticate')) {
-      return new Response(res.body, {
-        status: res.status,
-        headers: {
-          ...res.headers,
-          'Www-Authenticate': res.headers.get('Www-Authenticate').replace(/https?:\/\//, workerUrl),
-        },
-      });
+      const [_, realm, service, scope] = res.headers
+        .get('Www-Authenticate')
+        .match(/Bearer realm="([^"]+)",service="([^"]+)",scope="([^"]+)"/);
+      const auth = await fetch(new Request(`${realm}?service=${service}&scope=${scope}`));
+      if (auth.status === 200) {
+        const { token } = await auth.json();
+        req.headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      res = await fetch(req);
+      if (res.headers.get('Location')) {
+        res = await fetch(new Request(res.headers.get('Location'), req), { redirect: 'follow' });
+      }
     }
     return res;
   },
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,66 +1,80 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, test } from 'vitest';
 import worker from '../src';
 
-it.skip('hello', async () => {
-  const res = await fetch('https://example.com/');
+const BASE_URL = 'https://example.com';
+
+it('hello', async () => {
+  const res = await worker.fetch(new Request(BASE_URL));
   expect(res.status).toEqual(200);
   expect(await res.text()).toMatchSnapshot();
 });
 
-it.skip('Pull image from docker hub', async () => {
-  const headers = new Headers({
-    'accept-encoding': 'gzip, br',
-    'user-agent':
-      'docker/24.0.7 go/go1.20.10 git-commit/311b9ff kernel/6.6.9-200.fc39.x86_64 os/linux arch/amd64 UpstreamClient(Docker-Client/24.0.7 \\(linux\\))',
-  });
-  const v2Req = new Request('https://example.com/v2/', { headers });
-  const v2Res = await worker.fetch(v2Req);
-  expect(v2Res.status).toEqual(401);
-  expect(v2Res.headers.get('Www-Authenticate')).toMatchSnapshot();
-
-  const tokenReq = new Request(
-    'https://example.com/auth.docker.io/token/?scope=repository%3Alibrary%2Falpine%3Apull&service=registry.docker.io',
-    { headers }
-  );
-  const tokenRes = await worker.fetch(tokenReq);
-  expect(tokenRes.status).toEqual(200);
-  const tokenResult = await tokenRes.json();
-  expect(tokenResult).toHaveProperty('access_token');
-  expect(tokenResult).toHaveProperty('expires_in');
-  expect(tokenResult).toHaveProperty('issued_at');
-  expect(tokenResult).toHaveProperty('token');
-
-  const manifestReq = new Request('https://example.com/v2/library/alpine/manifests/latest', {
-    headers: {
-      ...headers,
-      Authorization: `Bearer ${tokenResult.access_token}`,
-    },
-  });
-  const manifestRes = await worker.fetch(manifestReq);
-  expect(manifestRes.status).toEqual(200);
-  const manifestResult = await manifestRes.json();
-  expect(manifestResult).toHaveProperty('name');
-  expect(manifestResult).toHaveProperty('fsLayers');
-  expect(manifestResult).toHaveProperty('history');
-  expect(manifestResult).toHaveProperty('tag');
+it('access /v2/ always returns 200', async () => {
+  const res = await worker.fetch(new Request(`${BASE_URL}/v2/`));
+  expect(res.status).toEqual(200);
+  expect(await res.json()).toEqual({});
 });
 
-it('Pull image from quay.io', async () => {
-  const headers = new Headers({
-    'accept-encoding': 'gzip, br',
-    'user-agent':
-      'docker/24.0.7 go/go1.20.10 git-commit/311b9ff kernel/6.6.9-200.fc39.x86_64 os/linux arch/amd64 UpstreamClient(Docker-Client/24.0.7 \\(linux\\))',
-  });
-  const v2Req = new Request('https://example.com/v2/quay.io', { headers });
-  const v2Res = await worker.fetch(v2Req);
-  expect(v2Res.status).toEqual(200);
+const headers = new Headers({
+  Accept:
+    'application/json, application/vnd.oci.image.manifest.v1+json, application/vnd.docker.distribution.manifest.v2+json, application/vnd.docker.distribution.manifest.list.v2+json, application/vnd.oci.image.index.v1+json, application/vnd.docker.distribution.manifest.v1+prettyjws',
+  'Accept-Encoding': 'br, gzip',
+  'User-Agent':
+    'docker/24.0.7 go/go1.20.10 git-commit/311b9ff kernel/6.6.9-200.fc39.x86_64 os/linux arch/amd64 UpstreamClient(Docker-Client/24.0.7 \\(linux\\))',
+});
 
-  const manifestReq = new Request('https://example.com/v2/quay.io/v2/minio/minio/manifests/latest', { headers });
-  const manifestRes = await worker.fetch(manifestReq);
-  expect(manifestRes.status).toEqual(200);
-  const manifestResult = await manifestRes.json();
-  expect(manifestResult).toHaveProperty('name');
-  expect(manifestResult).toHaveProperty('fsLayers');
-  expect(manifestResult).toHaveProperty('history');
-  expect(manifestResult).toHaveProperty('tag');
+/** @type {{repo: string, image: string, tag: string}[]} */
+const images = [
+  {
+    repo: 'library',
+    image: 'alpine',
+    tag: '3.20.2',
+  },
+  {
+    repo: 'docker.io/library',
+    image: 'alpine',
+    tag: '3.20.2',
+  },
+  {
+    repo: 'quay.io/prometheus',
+    image: 'busybox',
+    tag: 'latest',
+  },
+  {
+    repo: 'registry.k8s.io',
+    image: 'pause',
+    tag: '3.9',
+  },
+  {
+    repo: 'registry.k8s.io/ingress-nginx',
+    image: 'controller',
+    tag: 'v1.11.1',
+  },
+  {
+    repo: 'docker.elastic.co/elasticsearch',
+    image: 'elasticsearch',
+    tag: '7.17.9',
+  },
+  {
+    repo: 'ghcr.io/fatedier',
+    image: 'frps',
+    tag: 'v0.59.0',
+  },
+  {
+    repo: 'mcr.microsoft.com/devcontainers',
+    image: 'javascript-node',
+    tag: '1-18',
+  },
+];
+
+describe.each(images)('pull $image with $tag from $repo', async ({ repo, image, tag }) => {
+  it('head manifest', async () => {
+    const res = await worker.fetch(new Request(`${BASE_URL}/v2/${repo}/${image}/manifests/${tag}`, { headers, method: 'HEAD' }));
+    expect(res.status).toEqual(200);
+  });
+  it('get manifest', async () => {
+    const res = await worker.fetch(new Request(`${BASE_URL}/v2/${repo}/${image}/manifests/${tag}`, { headers }));
+    expect(res.status).toEqual(200);
+    expect(await res.json()).toMatchSnapshot();
+  });
 });
